e8a956a9f897affb54220cc084ee13534c1c1976
Domi <Domi>
2024-04-19 00:26:48 +0200

    codeFix

--- lib/home/style/styles.dart
+++ lib/home/style/styles.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
-// ** Custom Background ** //
+////////////// ** Custom Background ** //////////////
+
 const customBG = BoxDecoration(
   image: DecorationImage(
     image: AssetImage("assets/images/mainbg.png"),
     fit: BoxFit.cover,
   ),
 );
+
+////////////// ** End Custom Background ** ///////////////
